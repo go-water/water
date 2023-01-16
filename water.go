30cdb99dd13cfae5a8a27df3bb3f94eb9becfce4
pinbor <pinbor@iissy.com>
2023-01-16 11:52:28 +0800

    remove code

--- water.go
+++ water.go
@@ -2,12 +2,6 @@ package water
 
 type Service interface {
 	Endpoint() Endpoint
-	//DecodeRequest(ctx context.Context, req interface{}) (interface{}, error)
-	//EncodeResponse(ctx context.Context, resp interface{}) (interface{}, error)
-	//ServerBefore() ServerOption
-	//ServerAfter() ServerOption
-	//GetLogger() grpclog.DepthLoggerV2
-	//Name() string
 }
 
 func NewHandler(svc Service, options ...ServerOption) *Server {
