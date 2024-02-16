b17e0b0ca861f6d9388a796b0bbd22faa1e3f21e
pinbor <pinbor@go-water.cn>
2024-02-16 22:29:11 +0800

    优化日志的使用

--- logger/logger.go
+++ logger/logger.go
@@ -5,8 +5,8 @@ import (
 	"os"
 )
 
-func NewLogger() *slog.Logger {
-	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug, AddSource: true, ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
+func NewLogger(level slog.Leveler, addSource bool) *slog.Logger {
+	return slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: level, AddSource: addSource, ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
 		v := a.Value
 		if v.Kind() == slog.KindTime {
 			return slog.String(a.Key, v.Time().Format("2006-01-02 15:04:05"))
