a4daf9778d85418ec4300162c09a30105b3bfc45
pinbor <pinbor@go-water.cn>
2024-02-17 21:27:43 +0800

    修复命名

--- option.go
+++ option.go
@@ -7,22 +7,22 @@ import (
 	"time"
 )
 
-type ServerOption func(*adapter)
+type ServerOption func(h *handler)
 
 type ServerFinalizerFunc func(ctx context.Context, err error)
 
 func ServerFinalizer(f ...ServerFinalizerFunc) ServerOption {
-	return func(s *adapter) { s.finalizer = append(s.finalizer, f...) }
+	return func(h *handler) { h.finalizer = append(h.finalizer, f...) }
 }
 
 func ServerLimiter(interval time.Duration, b int) ServerOption {
-	return func(s *adapter) {
-		s.limit = rate.NewLimiter(rate.Every(interval), b)
+	return func(h *handler) {
+		h.limit = rate.NewLimiter(rate.Every(interval), b)
 	}
 }
 
 func ServerBreaker(breaker *gobreaker.CircuitBreaker) ServerOption {
-	return func(s *adapter) {
-		s.breaker = breaker
+	return func(h *handler) {
+		h.breaker = breaker
 	}
 }
