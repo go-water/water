a390a8f3c70db537f7144ed464308ef65fd499d8
pinbor <pinbor@go-water.cn>
2024-04-12 15:51:42 +0800

    修改变量命名

--- logger.go
+++ logger.go
@@ -3,17 +3,17 @@ package water
 import "github.com/go-water/water/logger"
 
 var (
-	l = logger.NewLogger(logger.Level, logger.AddSource)
+	log = logger.NewLogger(logger.Level, logger.AddSource)
 )
 
 func Info(msg string, args ...any) {
-	l.Info(msg, args...)
+	log.Info(msg, args...)
 }
 
 func Warn(msg string, args ...any) {
-	l.Warn(msg, args...)
+	log.Warn(msg, args...)
 }
 
 func Error(msg string, args ...any) {
-	l.Error(msg, args...)
+	log.Error(msg, args...)
 }
