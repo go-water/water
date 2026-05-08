676c06554ec31ee7dc664acc65db85bb07258bcd
pinbor <pinbor@jitask.com>
2026-05-08 22:44:56 +0800

    fix: add \n

--- water.go
+++ water.go
@@ -105,7 +105,7 @@ func (w *Water) print(addr string) {
 	fmt.Println("| | /| / / / __ `/ / __/ / _ \\  / ___/")
 	fmt.Println("| |/ |/ / / /_/ / / /_  /  __/ / /    ")
 	fmt.Println("|__/|__/ \\__,_/  \\__/  \\___/ /_/     ")
-	fmt.Printf("Listening and serving HTTP on %s", addr)
+	fmt.Printf("Listening and serving HTTP on %s\n", addr)
 }
 
 type Router struct {
